67722feaab65f7913f899b79acfc7a86bcce7fb2
Gunski <gunnarkryth@gmail.com>
2024-11-05 13:26:35 +0100

    Update ErrorMessage.jsx

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,5 +1,7 @@
 import Alert from "@mui/material/Alert";
 
-export const ErrorMessage = ({children}) => {
-  return <Alert severity="error">{children}</Alert>;
+export const ErrorMessage = ({ error }) => {
+  if (!error) return null;
+
+  return <Alert severity="error">{error}</Alert>;
 };
