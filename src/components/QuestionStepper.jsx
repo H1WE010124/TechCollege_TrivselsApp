f39301c93148a883fffec8951fbf47faf0f57068
kasperfryd <kfry@techcollege.dk>
2024-11-06 10:10:50 +0100

    added questionStepper component and react-icons

--- src/components/QuestionStepper.jsx
+++ src/components/QuestionStepper.jsx
@@ -1,24 +1,47 @@
-import {
-  MobileStepper as Mobile_Stepper,
-  Step,
-  StepButton,
-  StepConnector,
-  StepContent,
-  StepIcon,
-  StepLabel,
-  Stepper as MUI_Stepper,
-} from "@mui/material/Stepper";
+import { Button } from "@mui/material";
+import MobileStepper from "@mui/material/MobileStepper";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
 
-export const Stepper = () => {
+export const QuestionStepper = ({ steps }) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const theme = useTheme();
+
+  const handleNext = () => {
+    setActiveStep((prev) => prev + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
   return (
-    <Mobile_Stepper>
-      <Step>
-        <StepLabel />
-        <StepIcon />
-        <StepContent />
-        <StepButton />
-      </Step>
-      <StepConnector />
-    </Mobile_Stepper>
+    <MobileStepper
+      variant="progress"
+      steps={steps}
+      position={"static"}
+      activeStep={activeStep}
+      nextButton={
+        <Button
+          size="small"
+          variant="text"
+          onClick={handleNext}
+          disabled={activeStep === steps - 1}
+        >
+          Næste
+        </Button>
+      }
+      backButton={
+        <Button
+          size="small"
+          variant="text"
+          onClick={handleBack}
+          disabled={activeStep === 0}
+        >
+          <KeyboardArrowLeft /> Tilbage
+        </Button>
+      }
+    ></MobileStepper>
   );
 };
