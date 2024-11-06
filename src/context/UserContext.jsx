e21d1956fe7006e18e3486d113252a7195a02918
Razzoy <1081892@ucn.dk>
2024-11-06 10:30:27 +0100

    UserContext v.2

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
-export function UserContextProvider({children}){
+export function UserContextProvider({ children }) {
 
     //holder styr på brugerens access token
     const [accessToken, setAccessToken] = useState(null);
@@ -15,13 +15,20 @@ export function UserContextProvider({children}){
 
     //funktion der checker om access token allerede er i localstorage
     const checkAccessToken = () => {
-        return localStorage.getItem('accessToken',) !== null;
+        const token = localStorage.getItem("accessToken");
+        if (token !== null) {
+            console.log('Access token er allerede oprettet i localstorage');
+            return true;
+        } else {
+            console.log('Access token er ikke oprettet i localstorage');
+            return false;
+        }
     }
 
     //funktion der indlæser access token fra localstorage
     const loadAccessToken = () => {
         const token = localStorage.getItem('accessToken');
-        if (token){
+        if (token) {
             setAccessToken(token);
         }
     }
@@ -29,9 +36,10 @@ export function UserContextProvider({children}){
     //funktionen kaldes kun 1 gang, når komponentet mountes
     useEffect(() => {
         loadAccessToken();
+        checkAccessToken();
     }, [])
 
-    return(
+    return (
         <UserContext.Provider value={{ accessToken, saveAccessToken, checkAccessToken }}>
             {children}
         </UserContext.Provider>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { UserContextProvider } from './context/UserContext.jsx'
 import './index.css'
 
 const queryClient = new QueryClient();
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
