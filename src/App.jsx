204c9b2c705b7f45e13ce313d84054c12716906e
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-11-07 14:56:38 +0100

    #pulled from dev & added themes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,23 @@ import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import Button from '@mui/material/Button';
 import { ThemeProvider, createTheme } from "@mui/material"; 
-import { lightGreen } from "@mui/material/colors";
+import { green, lightGreen, purple } from "@mui/material/colors";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+import { QuestionStepper } from "./components/QuestionStepper/QuestionStepper";
+import { Question } from "./components/Question/Question";
+import { Clock } from "./components/Clock/Clock";
 
 
 const themeLight = createTheme({
   palette: {
     primary: {
-      main: lightGreen[400],
-    }
+      main: lightGreen[500],
+    },
+    secondary: {
+      main: purple[500]
+    },
   },
   typography: {
     fontFamily: 'sans-serif',
@@ -23,11 +32,20 @@ const themeLight = createTheme({
 
 const themeDark = createTheme({
   palette: {
+    primary: {
+      main: green[100],
+    },
     background: {
       default: "#222222"
     },
     text: {
       primary: '#ffffff'
+    },
+    action: {
+      disabled: '#8a8c7d'
+    },
+    button: {
+      backgroundColor: green[300]
     }
   }
 });
@@ -37,11 +55,17 @@ function App() {
   const [light, setLight] = useState(true);
 
   return (
-    //wrapping everything in ThemeProvider component so it affects everything and passing prop called theme and set it ecqual to which theme we want to provode to our app
+    <>
+    {/* wrapping everything in ThemeProvider component so it affects everything and passing prop called theme and set it ecqual to which theme we want to provode to our app */}
     <ThemeProvider theme={light ? themeLight : themeDark}>
-      <CssBaseline />
+      {/* CssBaseline inside ThemeProvider enables dark mode background */}
+      <CssBaseline  enableColorScheme />
       <Button onClick={() => setLight((prev) => !prev)}
       variant="contained">Theme</Button>
+      <Typography variant='poster'>      <Clock />      </Typography>
+      <Paper elevation={5}><Question /></Paper>
+      <QuestionStepper />
+      
       {/* <Button onClick={() => console.log('you clicked me')}
       variant="contained">Theme</Button> */}
       <div>
@@ -65,6 +89,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       </ThemeProvider>
+      </>
   );
 }
 
