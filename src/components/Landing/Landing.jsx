2a89d1e353375c04fee2ac0e3d498ac7fd4b247c
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-11-07 14:58:22 +0100

    Some more UI Material MUi stylings

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,11 @@
 import { Clock } from '../Clock/Clock';
 import styles from './Landing.module.scss';
 import {NavLink} from 'react-router-dom';
+import Box from '@mui/material/Box';
+
 
 export function Landing() {
+  
   return (
     <>
 
@@ -11,7 +14,12 @@ export function Landing() {
      </Box>
 
      <Box className={styles.ClockAndStart}>
-      <Box>
+      <Box
+      sx={{
+        color: '#2E7D32',
+        fontsize: rem (5),
+        marginBottom: rem (2),
+      }}>
       <Clock />
       </Box>
      <NavLink to={'/'}>Start &gt;</NavLink>
