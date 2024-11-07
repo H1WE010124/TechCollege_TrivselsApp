459459a4e2599e67aadb8adc9f0f89626c4345bc
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-11-07 09:39:32 +0100

    UnixToTimeFixed

--- src/helper/UnixToTime.jsx
+++ src/helper/UnixToTime.jsx
@@ -1,11 +1,7 @@
 
 
-export const UnixToTime = ({ unixTimestamp }) => {
-  const convertUnixToDate = (timestamp) => {
+ export const convertUnixToDate = (timestamp) => {
     return new Date(timestamp * 1000); 
   };
 
-  const date = convertUnixToDate(unixTimestamp);
-
-};
 
