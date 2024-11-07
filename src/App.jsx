2c9d8d7a26d642efb6ac0d83827c7b72cd5a9a2b
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-11-07 08:54:52 +0100

    cleaned up and starting from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 import { Landing } from './components/Landing/Landing';
 
 function App() {
   return (
     <>
-     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer
-
-        components={[
-          'MobileTimePicker'
-        ]}
-      >
-        <DemoItem label="Mobile variant">
-          <MobileTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
-        </DemoItem>
-      </DemoContainer>
-    </LocalizationProvider>
+     
     </>
   );
 }
