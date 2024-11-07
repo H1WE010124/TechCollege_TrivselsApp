9fd166b8a98c69dbadd40b511f255790ebdb56ca
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-11-07 09:57:44 +0100

    App.js, App.css, index.css is Default

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -6,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
   return (
     <>
       <div>
