77a8b664b698f0cc9eecd7b274aa765af818d7b3
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-11-07 09:55:17 +0100

    Clock is running automatically

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,15 +4,10 @@ import styles from './Landing.module.scss';
 export function Landing() {
   return (
     <>
-    <div className={styles.Admin}>
-    <button>Admin</button>
-    </div>
+
 
     <Clock></Clock>
 
-    <div className={styles.Start}>
-    <button>Start &gt;</button>
-    </div>
     
     
     </>
