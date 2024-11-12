0017171b6d032f78573674aa806f393221566cde
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-11-12 13:13:13 +0100

    Chart og dato

---
+++ src/pages/Chart.jsx
@@ -0,0 +1,12 @@
+import {DayjsTimezone} from "./../components/input/InputDate"
+import TickParamsSelector from './../components/Chart/Graph'
+
+export const GraphPage = () => {
+    return (
+        <>
+        <DayjsTimezone />
+        <TickParamsSelector />
+        </>
+    );
+   
+}
