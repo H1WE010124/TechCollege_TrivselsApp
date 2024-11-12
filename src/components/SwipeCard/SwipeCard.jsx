71e6663848472edba7d502aa843147687931efd9
amaliehausmann <amaliehausmann@gmail.com>
2024-11-12 10:30:50 +0100

    #79 swipe cards with props and conditional rendering based on screeen width

--- src/components/SwipeCard/SwipeCard.jsx
+++ src/components/SwipeCard/SwipeCard.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react'
-import style from './SwipeCard.module.scss'
-import TinderCard from 'react-tinder-card'
-import {QuestionCard} from '../QuestionCard/QuestionCard'
-import {YesNo} from '../YesNo/YesNo'
+import { useEffect } from "react";
+import style from "./SwipeCard.module.scss";
+import TinderCard from "react-tinder-card";
+import { QuestionCard } from "../QuestionCard/QuestionCard";
+import { YesNo } from "../YesNo/YesNo";
+import { useMediaQuery } from "@mui/material";
 
-export const SwipeCard = () => {
-    const [cards, setCards] = useState([{id: 1, question: 'Er du glad?'},]);
-    const [isDesktop, setIsDesktop] = useState(true);
+export const SwipeCard = ({
+  array,
+  callback,
+  currentQuestionIndex,
+  question,
+}) => {
+
+    const isDesktop = useMediaQuery('(min-width: 600px)')
+
+    useEffect(()=>{
+        console.log(isDesktop)
+    },[isDesktop])
 
   return (
-<>
-    <div>
-        {cards.map((item) => (
-                        <QuestionCard currentQuestionIndex={item.id - 1} totalQuestions={cards.length}>
-                        <p>{item.question}</p>
-                        <YesNo callback={()=> console.log('hej')}></YesNo>
-                    </QuestionCard>
-        ))}
-    </div>
-</>
-  )
-}
+    <>
+      {isDesktop && (
+        <QuestionCard
+          currentQuestionIndex={currentQuestionIndex}
+          totalQuestions={array?.length}
+        >
+          <p>{question}</p>
+          <YesNo callback={callback} />
+        </QuestionCard>
+      )}
+
+      {!isDesktop && (
+        <div className={style.cardContainer}>
+          {array && array
+            .slice()
+            .reverse()
+            .map((item, index) => (
+              <TinderCard
+                key={item.id}
+                className={style.swipe}
+                preventSwipe={["up", "down"]}
+              >
+                <div className={style.swipeCard}>
+                  <QuestionCard
+                    currentQuestionIndex={array.length - 1 - index}
+                    totalQuestions={array.length}
+                  >
+                    <p>{question}</p>
+                    <YesNo callback={callback} />
+                  </QuestionCard>
+                </div>
+              </TinderCard>
+            ))}
+        </div>
+      )}
+    </>
+  );
+};
