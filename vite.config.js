6dbbe00df2b622d561fbece05d53372b3c2a4fc2
Elena <rlenka2010gmail.com>
2024-11-13 08:33:56 +0100

    manifest

--- vite.config.js
+++ vite.config.js
@@ -8,21 +8,27 @@ export default defineConfig({
     registerType: 'autoUpdate', 
     includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
     manifest: {
-      name: 'TechPulse',
+      name: 'TechPulse App',
       short_name: 'App',
       description: 'Trivsel app til elever fra 8/9 klasse om en skoledag på TechCollege',
       theme_color: '#34C759', 
       background_color: '#ECFFED', 
       display: 'standalone', 
       start_url: '/', // URL for the PWA
+      scope: '/',
       icons: [
         {
-          src: 'icon.png',
+          src: '/Icon.png',
+          sizes: '96x96',
+          type: 'image/png'
+        },
+        {
+          src: '/Icon.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'icon.png',
+          src: '/Icon.png',
           sizes: '512x512',
           type: 'image/png'
         }
