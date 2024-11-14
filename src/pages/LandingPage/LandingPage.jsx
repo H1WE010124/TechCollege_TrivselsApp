98c79ba6ff1bb69d09bdb67dc0e91312fc5058c3
Michael P <102651473+michmich16@users.noreply.github.com>
2024-11-14 09:20:46 +0100

    added countdownText

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -8,23 +8,42 @@ import { UserContext } from "../../context/UserContext";
 
 export function LandingPage() {
   const [showStartButton, setShowStartButton] = useState(false);
+  const [countdownText, setCountdownText] = useState("");
 
-  // tjek om klok er mellem 12:00 and 14:00
+ // tjek om klok er mellem 12:00 and 14:00
   const checkTimeRange = () => {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
 
-    //12:00 - 12:59
     const isAfterStart = hours > 12 || (hours === 12 && minutes >= 0);
-    //13:00 - 13:59
     const isBeforeEnd = hours < 14 || (hours === 13 && minutes <= 59);
 
     return isAfterStart && isBeforeEnd;
   };
 
+  // regner ud om hvor lang tid er der til/fra kl. 12:00
+  const calculateCountdown = () => {
+    const now = new Date();
+    let targetTime = new Date();
+    targetTime.setHours(12, 0, 0, 0); // Set target time to 12:00
+
+    if (now >= targetTime) {
+      targetTime.setDate(targetTime.getDate() + 1); // sæt target til imorgen når kl 12 er forbi
+    }
+
+    const timeDifference = targetTime - now;
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+
+    setCountdownText(`Meningsmåling åbner om ${hours} timer og ${minutes} minutter`);
+  };
+
   useEffect(() => {
-    const updateButtonVisibility = () => setShowStartButton(checkTimeRange());
+    const updateButtonVisibility = () => {
+      setShowStartButton(checkTimeRange());
+      calculateCountdown();
+    };
     updateButtonVisibility();
 
     const intervalId = setInterval(updateButtonVisibility, 60000);
@@ -49,6 +68,13 @@ export function LandingPage() {
         >
           <Clock />
         </Box>
+        
+        {!showStartButton && (
+          <Box className={styles.CountdownText}>
+            {countdownText}
+          </Box>
+        )}
+
         {showStartButton && (
           <NavLink to={"/start"}>
             <AppButton buttonText={"Start"}></AppButton>
