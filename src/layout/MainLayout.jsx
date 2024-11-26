103b9770945c0e59e259502378edea35510afc60
kasperfryd <kfry@techcollege.dk>
2024-11-26 13:16:57 +0100

    fix styling charts

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,11 +3,18 @@ import { ThemeSwitcher } from "../components/ThemeSwitcher/ThemeSwitcher";
 import { Grid2, Box } from "@mui/material";
 import { AppButton } from "../components/AppButton/AppButton";
 import { BackButton } from "../components/BackButton/BackButton";
+import { useEffect } from "react";
 
 export const MainLayout = () => {
   const location = useLocation();
 
   console.log(location.pathname);
+
+  useEffect(() => {
+    document.title =
+      "TechPulse" +
+      (location.pathname !== "/" ? " - " + location.pathname.slice(1) : "");
+  }, [location]);
   return (
     <>
       <Grid2 gridTemplateRows={"1fr 10fr"} display="grid" height="95vh">
